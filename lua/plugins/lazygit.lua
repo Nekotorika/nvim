d2ead4c2596b6a30628b89d3316a17f51562de1f
Nekotorika <nakagawahauhausu@gmail.com>
2026-04-18 01:58:20 +0900

    lazygit.nvim fix

--- lua/plugins/lazygit.lua
+++ lua/plugins/lazygit.lua
@@ -8,13 +8,29 @@ return {
       "LazyGitFilter",
       "LazyGitFilterCurrentFile",
     },
-    -- 依存関係（ telescope.nvimを使っている場合は、拡張機能としても使えます）
     dependencies = {
       "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim", -- Telescopeを依存関係に追加
     },
-    -- キーマップの設定（例：<leader>gg で起動）
     keys = {
       { "<leader>gg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+      -- Telescope連携用のキーマップ（例：<leader>gt）
+      { "<leader>gt", "<cmd>Telescope lazygit<cr>", desc = "Telescope LazyGit" },
     },
+    config = function()
+      -- 前回のEscバグ対策
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = "lazygit",
+        callback = function()
+          vim.keymap.set("t", "<Esc>", "<Esc>", { buffer = true, silent = true })
+        end,
+      })
+
+      -- Telescope に lazygit エクステンションをロードさせる
+      local ok, telescope = pcall(require, "telescope")
+      if ok then
+        telescope.load_extension("lazygit")
+      end
+    end,
   },
 }
